ebd91fbe9391f1c74357f1f1eca8d112bb32e440
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2025-04-03 10:40:29 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,36 @@
+﻿namespace StudyGenericConstraintsLibrary
+{
+    //Generic constraint the class validates the type using "where T:typename"
+    public class GenericClass1<T> where T :struct
+    {
+        public T Msg;
+
+        public void GenericMethod(T p1)
+        {
+            Console.WriteLine($"Message = {Msg} ");
+            Console.WriteLine($"First Parameter= {p1}");
+            
+
+
+        }
+
+    }
+
+
+
+    public class GenericClass<T,X> where T: class where X : class
+    {
+     public T Msg;
+
+        public void GenericMethod(T p1, X p2,X p3)
+        {
+            Console.WriteLine($"Message = {Msg} ");
+            Console.WriteLine($"First Parameter= {p1}");
+            Console.WriteLine($"Second Parameter= {p2}");
+            Console.WriteLine($"Third Parameter= {p3}");
+
+
+        }
+
+    }
+}
---
+++ Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyGenericConstraintsLibrary
+{
+    public class Employee
+    {
+        public string City { get; set; }
+        public string Name { get; set; }
+    }
+    public class Manager : Employee 
+    {
+
+        public int Deptno { get; set; }
+        public string DeptName { get; set; }
+
+    }
+}
